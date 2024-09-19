a3d9b9d37590333ec8f704f34c77c088595cb3a5
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-09-19 23:15:29 +0300

    Add files via upload

---
+++ 2.1 back hw/main.py
@@ -0,0 +1,8 @@
+text= "Twinkle, Twinkle little star"
+for i in range(20):
+    print("Twinkle, Twinkle little star")
+for i in text:
+    print(i)
+for i in range(30):
+    if i%2==1:
+        print(i)
